6441c9a997170ca57c0f360d24dfa546da516406
José Reynaldo <ja.reynaldo@outlook.com>
2022-09-24 10:31:34 -0400

    finishing 01.14

---
+++ Module 01/Mod01 Assignments/01.13 Currency Conversion/Calc.java
@@ -0,0 +1,13 @@
+public class Calc {
+    public static void main(String[] args) {
+
+        int value = 7;
+        // calculations and output statements
+
+        System.out.println("The original value is " + value);
+        System.out.println("When " + value + " is devided by 2, the whole number result is " + (value / 2));
+        System.out.println("The remainder after the number " + value + " is devided by 2 is " + (value % 2));
+        System.out.println("When " + value + " is devided by 2, the decimal number result is " + ((double) value / 2));
+
+    }
+}
