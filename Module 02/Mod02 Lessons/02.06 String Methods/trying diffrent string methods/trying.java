a15a09e4dc9840c61fa731e588fdaab2d5675320
José Reynaldo <ja.reynaldo@outlook.com>
2022-10-12 11:09:09 -0400

    adding 2.07

---
+++ Module 02/Mod02 Lessons/02.06 String Methods/trying diffrent string methods/trying.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class trying {
+    public static void main(String[] args) {
+        String phone = "407-555-3326";
+        int indexOfDash = phone.indexOf("-");
+        String newPhone = "(" + phone.substring(0, indexOfDash) + ")" + phone.substring(indexOfDash + 1);
+        System.out.println(newPhone);
+        System.out.println();
+
+        String number = "4075553326";
+        String pt1 = number.substring(0, 3);
+        String pt2 = number.substring(3, 6);
+        String pt3 = number.substring(6);
+        String newNum = pt1 + "." + pt2 + "." + pt3;
+        System.out.println(newNum);
+
+        String str = "Shakespeare";
+        System.out.println(str.substring(2, 6));
+
+        String name = "Christopher Marlowe";
+        int x = name.indexOf("t") / 3;
+        String yesnan = name.substring(x, x + 3);
+        System.out.println(yesnan);
+
+        Scanner in;
+        in = new Scanner(System.in);
+        System.out.print("Please enter an integer value: ");
+        int intValue = in.nextInt();
+    }
+}
