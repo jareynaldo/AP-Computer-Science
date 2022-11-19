ab1127c245cc108d75e9384fc30c5c3b09ffbf1b
José Reynaldo <ja.reynaldo@outlook.com>
2022-11-19 12:57:58 -0500

    addig 3.8 DBA

---
+++ Module 03/Mod03 assigments/03.08 DBA/CarnivalTickets.java
@@ -0,0 +1,16 @@
+/**
+ * This program will determine the ticket price 
+ * for carnival admission based off a set of questions
+ * and generate an ID based off it. 
+ *
+ * @author Jose Reynaldo
+ * @version 11/12/2022
+ */
+import java.util.Scanner;
+
+public class CarnivalTickets {
+    public static void main(String[] args) {
+
+
+    }
+}
