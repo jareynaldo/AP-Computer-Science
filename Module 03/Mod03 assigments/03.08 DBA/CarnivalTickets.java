2045c9806e6ed6c1ef22bb8a0d87fce59c985a83
José Reynaldo <ja.reynaldo@outlook.com>
2022-11-22 11:03:19 -0500

    finishing 03.08

--- Module 03/Mod03 assigments/03.08 DBA/CarnivalTickets.java
+++ Module 03/Mod03 assigments/03.08 DBA/CarnivalTickets.java
@@ -12,33 +12,41 @@ import java.util.Scanner;
 public class CarnivalTickets {
 
     public static void printTicket(String name, String id, double price) {
-
+        System.out.println("#############################################");
+        System.out.println();
         System.out.println("Welcome to the APCS Carnival, " + name + "!");
         System.out.println("Your user ID is  " + id);
         System.out.println("The cost of your ticket is $" + price + ".");
         System.out.println("Have a great time at the APCS Carnival today!");
-
+        System.out.println();
+        System.out.println("#############################################");
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Hello! Please input your name(First Last):   ");
+        System.out.print("Hello! Please input your name(First Last): ");
         String name = in.nextLine();
-        System.out.println("How old are you?  ");
 
-        int age = Integer.parseInt(in.nextLine());
-        System.out.print("Are you a veteran(Y or N)?   ");
+        System.out.print("How old are you? ");
+        String askedAge = in.nextLine();
+        int age = Integer.parseInt(askedAge);
+
+        System.out.print("Are you a veteran(Y or N)? ");
         String veteran = in.nextLine();
-        System.out.print("Are you a First Responder(Y or N)?   ");
+
+        System.out.print("Are you a First Responder(Y or N)? ");
         String firstResponder = in.nextLine();
 
-        Boolean discount = false;
-        if (firstResponder == "Y" || veteran == "Y") {
+        System.out.println();
+        System.out.println();
+
+        Boolean discount = false;// if dicount is needeed
+        if (firstResponder.equalsIgnoreCase("y") || veteran.equalsIgnoreCase("y")) {
             discount = true;
         }
 
-        double ticketPrice;
+        double ticketPrice;// figuering out ticket price w/o discount
         if (age < 3) {
             ticketPrice = 2;
         } else if (age < 6) {
@@ -48,19 +56,15 @@ public class CarnivalTickets {
         } else {
             ticketPrice = 12;
         }
-        // if ((firstResponder == "Y" || veteran == "Y") && age > 18) {
-        // ticketPrice *= .5;
-        // }
+
         if (discount && age > 18) {
             ticketPrice *= .5;
         }
 
         int index = name.indexOf(" ") + 1;
         String id = name.substring(0, 1) + name.substring(index) + (int) (Math.random() * 1000);
-        System.out.println(id);
-        System.out.println(ticketPrice);
 
-        // printTicket(null, null, 0);
+        printTicket(name, id, ticketPrice);
 
     }
 }
