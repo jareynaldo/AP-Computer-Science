e9c311b9f612f4bb7e75de465aaaf93dfe78c26d
José Reynaldo <ja.reynaldo@outlook.com>
2022-11-24 15:42:38 -0500

    starting 4.6

--- Module 04/Mod04 Lesson/04.02 Iterations with While Loops/ConvertFeetToInches.java
+++ Module 04/Mod04 Lesson/04.02 Iterations with While Loops/ConvertFeetToInches.java
@@ -7,36 +7,43 @@
  * @version 06/05/17
  */
 import java.util.Scanner;
-public class ConvertFeetToInches
-{
-    public static void main(String[] args)
-    {
-        // Declare and initialize variables
-        String feetStr = "";
-        double feet = 0;
-        double inches = 0;
 
-        Scanner in = new Scanner(System.in);
+public class ConvertFeetToInches {
+    public static void main(String[] args) {
+        // // Declare and initialize variables
+        // String feetStr = "";
+        // double feet = 0;
+        // double inches = 0;
 
-        String convertAnother = "Y";
+        // Scanner in = new Scanner(System.in);
 
-        //notice the use of negating the equalsIgnoreCase() method
-        //to terminate the loop
-        while(!convertAnother.equalsIgnoreCase("N"))
-        {
-            System.out.print("Enter Feet: ");
-            feetStr = in.nextLine();     //What happens if a user enters an int, a double, or a String?
-            feet = Double.parseDouble(feetStr);  
+        // String convertAnother = "Y";
 
-            inches = 12 * feet;
-            System.out.println("Inches: " + inches);
-            System.out.println();
-            System.out.print("Convert another measurement (Y or N): ");
-            convertAnother = in.nextLine();   //What happens if you enter a letter other than Y or N?
-        }//end while
+        // //notice the use of negating the equalsIgnoreCase() method
+        // //to terminate the loop
+        // while(!convertAnother.equalsIgnoreCase("N"))
+        // {
+        // System.out.print("Enter Feet: ");
+        // feetStr = in.nextLine(); //What happens if a user enters an int, a double, or
+        // a String?
+        // feet = Double.parseDouble(feetStr);
 
-        System.out.println("Thank you!");
+        // inches = 12 * feet;
+        // System.out.println("Inches: " + inches);
+        // System.out.println();
+        // System.out.print("Convert another measurement (Y or N): ");
+        // convertAnother = in.nextLine(); //What happens if you enter a letter other
+        // than Y or N?
+        // }//end while
+
+        // System.out.println("Thank you!");
+
+        String phoneNum1 = "407-555-2207";
+        String phoneNum2 = "407-555-5000";
+
+        if (phoneNum1.compareTo(phoneNum2) > 0) {
+            System.out.print(1);
+        }
+
     }
 }
-
-
---
+++ Module 04/Mod04 Lesson/04.06 File Output/SecretPasscode.java
@@ -0,0 +1,63 @@
+
+/**
+ * Randomly generate passwords.
+ *
+ * @author Jose Reynaldo
+ * @version 11/23/22
+ * 
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.File;
+import java.util.Scanner;
+
+public class SecretPasscode {
+    public static void main(String[] args) throws IOException {
+        String userChoice = "Y";
+        int pwLength = 0;
+        int randNum = 0;
+        String password = "";
+
+        // initialize scanner and print writer
+        Scanner in = new Scanner(System.in);
+
+        // construct an object called outFile to allow access to output methods of the
+        // PrintWriter class
+        PrintWriter outFile = new PrintWriter(new File("pass.txt"));
+
+        System.out.println("**********************************************************");
+        System.out.println(
+                "Welcome to a simple password generator. \nThe password will include lowercase, uppercase, & numbers.");
+
+        // the user wants to continue
+        System.out.print("Enter a password Length ( 6 or more): ");
+        int passLength = in.nextInt();
+
+        while (passLength < 6) {
+            System.out.println("Password length too short. Please try again.");
+            System.out.print("Enter a password Length ( 6 or more): ");
+            passLength = in.nextInt();
+
+        }
+
+        int randomNum;
+        for (int i = 0; i < passLength; i++) {
+            randomNum = (int) (Math.random() * 255) + 33;
+            password += (char) randomNum;
+        }
+        outFile.println(password);
+
+        outFile.close();
+        // generate a random character in the correct character set
+
+        // concatenate the character to the password
+
+        // write the password to the file
+
+        // when the user is done, close the text file so that it can be re-opened and
+        // read
+
+        // Read passwords back from text file and display to the screen
+
+    }// end main
+}// end class
